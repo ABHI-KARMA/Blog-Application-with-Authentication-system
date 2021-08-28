e56e39db7a4485f74870aa5c38c4fca856e8dd85
ABHISHEK VISHWAKARMA <73927352+ABHI-KARMA@users.noreply.github.com>
2021-08-27 20:58:50 -0700

    update settings.py

--- Pro/Pro/settings.py
+++ Pro/Pro/settings.py
@@ -127,7 +127,7 @@ EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_USE_TLS = True
 EMAIL_PORT = 587
-EMAIL_HOST_USER = 'abhivishwakarma1409@gmail.com'
-EMAIL_HOST_PASSWORD  = 'ABHI-KARMA'
+EMAIL_HOST_USER = '###############################' #Host Email Address
+EMAIL_HOST_PASSWORD  = '#################' # Host Email's Password
 
 LOGIN_URL = '/login/'
